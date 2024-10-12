67a4dc0c86aaae82be4cfec74e2457e58ecb82f1
EdmundGelting <edmundgelting@proton.me>
2024-10-12 17:54:19 +0200

    Quartz sync: Oct 12, 2024, 5:54 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -19,8 +19,6 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
@@ -30,7 +28,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
