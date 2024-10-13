adf4be4828180641619817ee58d88298fbfb13b3
Master-Noiza <168280380+Master-Noiza@users.noreply.github.com>
2024-10-13 14:27:31 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,7 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({ folderClickBehavior: "link", })),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
