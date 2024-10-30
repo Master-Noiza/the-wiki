44255baf746e9770a7c5c40912c08c5df028e222
Master-Noiza <168280380+Master-Noiza@users.noreply.github.com>
2024-10-30 14:38:51 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,7 +24,38 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+  sortFn: (a, b) => {
+    const nameOrderMap: Record<string, number> = {
+      "The Magic": 100,
+      "Atlas": 200,
+      "Gods & Religion": 300,
+      "The Races": 400,
+      "Dramatis Personae": 500,
+      "Organizations": 600,
+      "Herbarium": 700,
+      "Mechanics": 800,
+      "OOC": 900,
+    }
+ 
+    let orderA = 0
+    let orderB = 0
+ 
+    if (a.file && a.file.slug) {
+      orderA = nameOrderMap[a.file.slug] || 0
+    } else if (a.name) {
+      orderA = nameOrderMap[a.name] || 0
+    }
+ 
+    if (b.file && b.file.slug) {
+      orderB = nameOrderMap[b.file.slug] || 0
+    } else if (b.name) {
+      orderB = nameOrderMap[b.name] || 0
+    }
+ 
+    return orderA - orderB
+  },
+})),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
