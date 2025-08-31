ef36a44472a1f7aa06e72d2345d037dffb4caec8
Master-Noiza <168280380+Master-Noiza@users.noreply.github.com>
2025-08-31 22:11:29 +0200

    Update contentIndex.ts
    Also trying to add the frontmatter

--- quartz/plugins/emitters/contentIndex.ts
+++ quartz/plugins/emitters/contentIndex.ts
@@ -135,6 +135,8 @@ export const ContentIndex: QuartzEmitterPlugin<Partial<Options>> = (opts) => {
               : undefined,
             date: date,
             description: file.data.description ?? "",
+            // mod: add the original frontmatter as whole
+            frontmatter: file.data.frontmatter,
           })
         }
       }
